75c6041c83c426c157a9b730eb8dc700fb81534b
unknown <dimaskupejko@gmail.com>
2021-03-09 15:43:23 +0200

    DirectX 11: Tutorial 4: not finished

---
+++ cameraclass.cpp
@@ -0,0 +1,25 @@
+//////////////////////////////////
+// Fileanem: cameraclass.cpp
+//////////////////////////////////
+#include "cameraclass.h"
+
+CameraClass::CameraClass()
+{
+	m_positionX = 0.0f;
+	m_positionY = 0.0f;
+	m_positionZ = 0.0f;
+
+	m_rotationX = 0.0f;
+	m_rotationY = 0.0f;
+	m_rotationZ = 0.0f;
+}
+
+CameraClass::CameraClass(const CameraClass& other)
+{
+}
+
+CameraClass::~CameraClass()
+{
+}
+
+void CameraClass::SetPosition(float x, float y,)
