07414d139db4bf3907c614fe1756ad090afda264
unknown <dimaskupejko@gmail.com>
2021-03-18 17:57:51 +0200

    DirectX 11: Tutorial 6: #2 not finished

---
+++ lightshaderclass.h
@@ -0,0 +1,52 @@
+//////////////////////////////////
+// Filename: lightshaderclass.h
+//////////////////////////////////
+#ifndef _LIGHTSHADERCLASS_H_
+#define _LIGHTSHADERCLASS_H_
+
+//////////////////////////////////
+// INCLUDES
+//////////////////////////////////
+#include <D3D11.h>
+#include <D3DX10math.h>
+#include <D3DX11async.h>
+#include <fstream>
+
+//////////////////////////////////
+// Class name: LightShaderClass
+//////////////////////////////////
+class LightShaderClass
+{
+public:
+	LightShaderClass();
+	LightShaderClass(const LightShaderClass&);
+	~LightShaderClass();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX,
+				ID3D11ShaderResourceView*, D3DXVECTOR3, D3DXVECTOR4);
+
+private:
+	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*)
+		
+		В статті знайди: Существует несколько функций для конвертирования Мульбайтовых строк в UNICODE, о которых я скоро расскажу.
+		і читай далі
+private:
+	struct MatrixBufferType
+	{
+		D3DXMATRIX world;
+		D3DXMATRIX view;
+		D3DXMATRIX projection;
+	};
+
+	struct LightBufferType
+	{
+		D3DXVECTOR4 diffuseColor;
+		D3DXVECTOR3 lightDirection;
+		float padding;		// Added extra padding so structure is a multiple
+							// of 16 for CreateBuffer function requirements
+	};
+};
+
+#endif
