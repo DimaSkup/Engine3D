4fa0bb81835e976beec54e4828af44ce01f807f1
unknown <dimaskupejko@gmail.com>
2021-04-01 17:55:19 +0300

    DirectX 11: Tutorial 8: #3 not finished

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 #include <iostream>
 #include <fstream>
 
+#include "systemclass.h"
+
+using namespace std;
+
+
 //////////////////////////////////
 // TYPEDEFS
 //////////////////////////////////
@@ -170,7 +175,192 @@ bool ReadFileCounts(char* filename, int& vertexCount, int& textureCount,
 	return true;
 }
 
-bool LoadDataStructures
+bool LoadDataStructures(char* filename, int vertexCount, int textureCount, int normalCount, int faceCount)
+{
+	VertexType *vertices, *texcoords, *normals;
+	FaceType *faces;
+	ifstream fin;
+	int vertexIndex, texcoordIndex, normalIndex, faceIndex, vIndex, tIndex, nIndex;
+	char input, input2;
+	ofstream fout;
+
+	// Initialize the four data structures
+	vertices = new VertexType[vertexCount];
+	if (!vertices)
+	{
+		return false;
+	}
+
+	texcoords = new VertexType[textureCount];
+	if (!texcoords)
+	{
+		return false;
+	}
+
+	normals = new VertexType[normalCount];
+	if (!normals)
+	{
+		return false;
+	}
+
+	faces = new FaceType[faceCount];
+	if (!faces)
+	{
+		return false;
+	}
+
+
+	// Initialize the indexes
+	vertexIndex = 0;
+	texcoordIndex = 0;
+	normalIndex = 0;
+	faceIndex = 0;
+
+	// Open the file
+	fin.open(filename);
+
+	// Check if it was successful in opening the file
+	if (fin.fail() == true)
+	{
+		return false;
+	}
+
+	// Read in the vertices, texture coordinates, and normals into the data structures.
+	// Important: Also convert to left hand coordinate system since Maya uses right hand coordinate system.
+	fin.get(input);
+	while (!fin.eof())
+	{
+		if (input == 'v')
+		{
+			fin.get(input);
+
+			// Read in the vertices
+			if (input == ' ')
+			{
+				fin >> vertices[vertexIndex].x >> vertices[vertexIndex].y >> vertices[vertexIndex].z;
+
+				// Invert the Z vertex to change to left hand system
+				vertices[vertexIndex].z = vertices[vertexIndex].z * -1.0f;
+				vertexIndex++;
+			}
+
+			//Read in the texture uv coordinates
+			if (input == 't')
+			{
+				fin >> texcoords[texcoordIndex].x >> texcoords[texcoordIndex].y;
+
+				// Invert the V texture coordinates to left hand system
+				texcoords[texcoordIndex].y = 1.0f - texcoords[texcoordIndex].y;
+				texcoordIndex++;
+			}
+
+			// Read in the normals
+			if (input = 'n')
+			{
+				fin >> normals[normalIndex].x >> normals[normalIndex].y >> normals[normalIndex].z;
+
+				// Invert the Z normal to change to left hand system
+				normals[normalIndex].z = normals[normalIndex].z * -1.0f;
+				normalIndex++;
+			}
+		}
+
+		// Read in the faces
+		if (input = 'f')
+		{
+			fin.get(input);
+			
+			if (input == ' ')
+			{
+				// Read the face data in backwards to convert it to a left hand system from right hand system
+				fin >> faces[faceIndex].vIndex3 >> input2 >> faces[faceIndex].tIndex3 >> input2 >> faces[faceIndex].nIndex3
+					>> faces[faceIndex].vIndex2 >> input2 >> faces[faceIndex].tIndex2 >> input2 >> faces[faceIndex].nIndex2
+					>> faces[faceIndex].vIndex1 >> input2 >> faces[faceIndex].tIndex1 >> input2 >> faces[faceIndex].nIndex1;
+
+				faceIndex++;
+			}
+		}
+
+		// Read in the remainder of the line
+		while (input != '\n')
+		{
+			fin.get(input);
+		}
+
+		// Start reading the beginning of the next line
+		fin.get(input);
+	}
+
+	// Close the file
+	fin.close();
+
+	// Open the output file
+	fout.open("model.txt");
+
+	// Write out the file header that our model format uses
+	fout << "Vertex Count: " << (faceCount * 3) << endl;
+	fout << endl;
+	fout << "Data:" << endl;
+	fout << endl;
+
+	// Now loop through all the faces and output the three vertices for each face
+	for (int i = 0; i < faceIndex; i++)
+	{
+		vIndex = faces[i].vIndex1 - 1;
+		tIndex = faces[i].tIndex1 - 1;
+		nIndex = faces[i].nIndex1 - 1;
+
+		fout << vertices[vIndex].x << ' ' << vertices[vIndex].y << ' ' << vertices[vIndex].z << ' '
+			 << texcoords[tIndex].x << ' ' << texcoords[tIndex].y << ' '
+			 << normals[nIndex].x << ' ' << normals[nIndex].y << ' ' << normals[nIndex].z << endl;
+
+		vIndex = faces[i].vIndex2 - 1;
+		tIndex = faces[i].tIndex2 - 1;
+		nIndex = faces[i].nIndex2 - 1;
+
+		fout << vertices[vIndex].x << ' ' << vertices[vIndex].y << ' ' << vertices[vIndex].z << ' '
+			 << texcoords[tIndex].x << ' ' << texcoords[tIndex].y << ' '
+			 << normals[nIndex].x << ' ' << normals[nIndex].y << ' ' << normals[nIndex].z << endl;
+
+		vIndex = faces[i].vIndex3 - 1;
+		tIndex = faces[i].tIndex3 - 1;
+		nIndex = faces[i].nIndex3 - 1;
+
+		fout << vertices[vIndex].x << ' ' << vertices[vIndex].y << ' ' << vertices[vIndex].z << ' '
+			 << texcoords[tIndex].x << ' ' << texcoords[tIndex].y << ' '
+			 << normals[nIndex].x << ' ' << normals[nIndex].y << ' ' << normals[nIndex].z << endl;
+	} 
+
+	// Close the output file
+	fout.close();
+
+	// Release the four data structures
+	if (vertices)
+	{
+		delete[] vertices;
+		vertices = nullptr;
+	}
+
+	if (texcoords)
+	{
+		delete[] texcoords;
+		texcoords = nullptr;
+	}
+
+	if (normals)
+	{
+		delete[] normals;
+		normals = nullptr;
+	}
+
+	if (faces)
+	{
+		delete[] faces;
+		faces = nullptr;
+	}
+
+	return true;
+}
 
 
 
@@ -181,8 +371,8 @@ bool LoadDataStructures
 
 
 
-#include "systemclass.h"
 
+/*
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
 {
 	SystemClass *System;
@@ -211,4 +401,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline,
 	System = nullptr;
 
 	return 0;
-}
+}*/
