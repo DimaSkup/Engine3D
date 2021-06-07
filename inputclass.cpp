785cddda5cd293f4f49a001e6296bc09040803d8
unknown <dimaskupejko@gmail.com>
2021-06-07 12:45:44 +0300

    DirectX 11: Tutorial 13: #1 not finished

--- inputclass.cpp
+++ inputclass.cpp
@@ -5,6 +5,9 @@
 
 InputClass::InputClass()
 {
+	m_directInput = nullptr;
+	m_keyboard = nullptr;
+	m_mouse = nullptr;
 }
 
 InputClass::InputClass(const InputClass& other)
@@ -15,39 +18,190 @@ InputClass::~InputClass()
 {
 }
 
-void InputClass::Initialize()
+bool InputClass::Initialize(HINSTANCE hinstance, HWND hwnd, 
+							int screenWidth, int screenHeight)
 {
-	int i;
+	HRESULT result;
 
-	// Initialize all the keys to being released and not pressed
-	for(int i = 0; i < 256; i++)
+	// Store the screen size which will be used for positioning the mouse cursor
+	m_screenWidth = screenWidth;
+	m_screenHeight = screenHeight;
+
+	// Initialize the location of the mouse on the screen
+	m_mouseX = 0;
+	m_mouseY = 0;
+
+	// Initalize the main direct input interface
+	result = DirectInput8Create(hinstance, DIRECTINPUT_VERSION, 
+								IID_IDirectInput8, (void**)&m_directInput, NULL);
+	if (FAILED(result))
 	{
-		m_keys[i] = false;
+		return false;
+	}
+
+	// Initialize the direct input interface for the keyboard
+	result = m_directInput->CreateDevice(GUID_SysKeyboard, &m_keyboard, NULL);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// Set the data format. In this case since it is a keyboard we can use 
+	// the predefined data format
+	result = m_keyboard->SetDataFormat(&c_dfDIKeyboard);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// Set the cooperative level of the keyboard to not share with other programs
+	result = m_keyboard->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_EXCLUSIVE);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// Now acquire the keyboard
+	result = m_keyboard->Acquire();
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// Initialize the direct input interface for the mouse
+	result = m_directInput->CreateDevice(GUID_SysMouse, &m_mouse, NULL);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// Set the data format for the mouse using the pre-defined mouse data format
+	result = m_mouse->SetDataFormat(&c_dfDIMouse);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// Set the cooperative level for the mouse to shader with other programs
+	result = m_mouse->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+// Acquire the mouse
+result = m_mouse->Acquire();
+if (FAILED(result))
+{
+	return false;
+}
+
+return true;
+}
+
+
+void InputClass::Shutdown()
+{
+	// Release the mouse
+	if (m_mouse)
+	{
+		m_mouse->Unacquire();
+		m_mouse->Release();
+		m_mouse = nullptr;
+	}
+
+	// Release the keyboard
+	if (m_keyboard)
+	{
+		m_keyboard->Unacquire();
+		m_keyboard->Release();
+		m_keyboard = nullptr;
+	}
+
+	// Release the main interface to direct input
+	if (m_directInput)
+	{
+		m_directInput->Release();
+		m_directInput = nullptr;
 	}
 
 	return;
 }
 
 
-void InputClass::KeyDown(unsigned int input)
+bool InputClass::Frame()
 {
-	// If a key is pressed then save state in the key array
-	m_keys[input] = true;
+	bool result;
 
-	return;
+	// Read the current state of the keyboard
+	result = ReadKeyboard();
+	if (!result)
+	{
+		return false;
+	}
+
+	// Read the current state of the mouse
+	result = ReadMouse();
+	if (!result)
+	{
+		return false;
+	}
+
+	// Process the changes in the mouse and keyboard
+	ProcessInput();
+
+	return true;
 }
 
-void InputClass::KeyUp(unsigned int input)
-{
-	// If a key is released then clear that state in the key array
-	m_keys[input] = false;
 
-	return;
+bool InputClass::ReadKeyboard()
+{
+	HRESULT result;
+
+	// Read the keyboard device
+	result = m_keyboard->GetDeviceState(sizeof(m_keyboardState),
+		(LPVOID)&m_keyboardState);
+	if (FAILED(result))
+	{
+		// If the keyboard lost focus or was not acquire then try to get control back
+		if ((result == DIERR_INPUTLOST) || (result == DIERR_NOTACQUIRED))
+		{
+			m_keyboard->Acquire();
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
 
-bool InputClass::IsKeyDown(unsigned int key)
+
+bool InputClass::ReadMouse()
 {
-	// Return what state the key is in (pressed/not pressed)
-	return m_keys[key];
+	HRESULT result;
+
+	// Read the mouse device
+	result = m_mouse->GetDeviceState(sizeof(DIMOUSESTATE), (LPVOID)&m_mouseState);
+	if (FAILED(result))
+	{
+		// If the mouse lost focus or was not acquired then try to get control back
+		if ((result == DIERR_INPUTLOST) || (result == DIERR_NOTACQUIRED))
+		{
+			m_mouse->Acquire();
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
 
+
+void InputClass::ProcessInput()
+{
+
+}
