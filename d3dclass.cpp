ae4fa31ca1b3a5a2e9428175d14206d3cf9a7056
unknown <dimaskupejko@gmail.com>
2022-03-24 01:55:17 +0200

    Tutorial 3: #15: revising of all the D3DClass

--- d3dclass.cpp
+++ d3dclass.cpp
@@ -3,11 +3,12 @@
 // Here we initialize the Direct3D
 /////////////////////////////////////////////////////////////////////
 #include "d3dclass.h"
+#define THIS_FUNC "%s(): %s",__FUNCTION__
 
 // Empty constructor
 D3DClass::D3DClass(void)
 {
-	Log::Get()->Debug(__FUNCTION__);
+	Log::Get()->Debug(THIS_FUNC, "");
 
 	m_pSwapChain = nullptr;
 	m_pDevice = nullptr;
@@ -38,10 +39,10 @@ D3DClass::~D3DClass(void)
 
 // this function initializes Direct3D
 bool D3DClass::Initialize(int screenWidth, int screenHeight, bool VSYNC_ENABLED,
-	HWND hwnd, bool FULL_SCREEN,
-	float screenNear, float screenDepth)
+							HWND hwnd, bool FULL_SCREEN,
+							float screenNear, float screenDepth)
 {
-	Log::Get()->Debug("%s:%s", __FUNCTION__, "the beginning");
+	Log::Get()->Debug(THIS_FUNC, "the beginning");
 	HRESULT hr = S_OK;
 
 	// DXGI variables, etc
@@ -50,10 +51,23 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool VSYNC_ENABLED,
 	IDXGIOutput*  output = nullptr;		// a pointer to interface of the display output adapter 
 	DXGI_ADAPTER_DESC adapterDesc;		// contains description of the adapter (video card)
 	DXGI_MODE_DESC* displayModeList = nullptr;	// a pointer to the list of display adapter modes
-	UINT numModes = 0;					// a number of dispay modes
-	int numerator = 0, denominator = 0;	// numerator and denominator of the display refresh rate
-	UINT error = 0;						// info about errors of conterting of WCHAR line into simple char line
+	UINT numModes = 0;							// a number of dispay modes
+	UINT numerator = 0, denominator = 0;		// numerator and denominator of the display refresh rate
+	UINT error = 0;								// info about errors of conterting of WCHAR line into simple char line
 	size_t stringLength = 0;
+
+	// DirectX stuff
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	D3D_FEATURE_LEVEL  featureLevel;
+	UINT createDeviceFlags = 0;
+	ID3D11Texture2D* pBackBuffer = nullptr;
+	D3D11_TEXTURE2D_DESC depthStencilBufferDesc;
+	D3D11_DEPTH_STENCIL_DESC depthStencilDesc;
+	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc;
+
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
 
 	// define if VSYNC is enabled or not
 	m_vsync_enabled = VSYNC_ENABLED;
@@ -62,7 +76,7 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool VSYNC_ENABLED,
 	hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&factory);
 	if (FAILED(hr))
 	{
-		Log::Get()->Error("%s:%s", __FUNCTION__, "can't create the DXGI Factory");
+		Log::Get()->Error(THIS_FUNC, "can't create the DXGI Factory");
 		return false;
 	}
 
@@ -70,7 +84,7 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool VSYNC_ENABLED,
 	hr = factory->EnumAdapters(0, &adapter);
 	if (FAILED(hr))
 	{
-		Log::Get()->Error("%s%s", __FUNCTION__, "can't enumerate adapters (video cards)");
+		Log::Get()->Error(THIS_FUNC, "can't enumerate adapters (video cards)");
 		return false;
 	}
 
@@ -78,7 +92,7 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool VSYNC_ENABLED,
 	hr = adapter->EnumOutputs(0, &output);
 	if (FAILED(hr))
 	{
-		Log::Get()->Error("%s:%s", __FUNCTION__, "can't enumerate ouput adapters (display adapters)");
+		Log::Get()->Error(THIS_FUNC, "can't enumerate ouput adapters (display adapters)");
 		return false;
 	}
 
@@ -89,7 +103,7 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool VSYNC_ENABLED,
 	hr = output->GetDisplayModeList(format, flags, &numModes, nullptr);
 	if (FAILED(hr))
 	{
-		Log::Get()->Error("%s:%s", __FUNCTION__, "can't get the number of display modes");
+		Log::Get()->Error(THIS_FUNC, "can't get the number of display modes");
 	}
 
 	// allocate the memory for the display modes description list
@@ -99,18 +113,197 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool VSYNC_ENABLED,
 	hr = output->GetDisplayModeList(format, flags, &numModes, displayModeList);
 	if (FAILED(hr))
 	{
-		Log::Get()->Error("%s:%s", __FUNCTION__, "can't initialize the display mode list");
+		Log::Get()->Error(THIS_FUNC, "can't initialize the display mode list");
 	}
 
 	// look for a mode which has the necessary screen resolution and get its refresh rate 
 	for (size_t i = 0; i < numModes; i++)
 	{
-		if (displayModeList[i].Width == static_cast<UINT>)
+		if (displayModeList[i].Width == static_cast<UINT>(screenWidth) &&
+			displayModeList[i].Height == static_cast<UINT>(screenHeight))
+		{
+			numerator = displayModeList[i].RefreshRate.Numerator;
+			denominator = displayModeList[i].RefreshRate.Denominator;
+		}
+	}
+
+	// get description of the adapter (video card)
+	hr = adapter->GetDesc(&adapterDesc);
+	if (FAILED(hr))
+	{
+		Log::Get()->Error(THIS_FUNC, "can't get description of the adapter (video card)");
+		return false;
+	}
+
+	// get the video card memory amount in megabytes
+	m_videoCardMemory = adapterDesc.DedicatedVideoMemory / 1024 / 1024;
+
+	// get the video card name
+	error = wcstombs_s(&stringLength, m_videoCardDescription, 128, adapterDesc.Description, 128);
+	if (error != 0)
+	{
+		Log::Get()->Error(THIS_FUNC, "can't convert the video card description from WCHAR type into char line");
+		return false;
 	}
 
 
+	// clear the memory from DXGI variables
+	_DELETE(displayModeList);
+	_RELEASE(output);
+	_RELEASE(adapter);
+	_RELEASE(factory);
+
+	// check the data
+	Log::Get()->Debug("video card memory      = %d MB", m_videoCardMemory);
+	Log::Get()->Debug("video card name        = %s", m_videoCardDescription);
+	Log::Get()->Debug("video card refreshRate = %d:%d", numerator, denominator);
 
 
+	// ------------------------------------------------------------------------------ //
+	//             CREATE THE SWAP CHAIN, DEVICE AND DEVICE CONTEXT                   //
+	// ------------------------------------------------------------------------------ //
+
+	// Initialize the swap chain description
+	ZeroMemory(&swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	// Setup the swap chain description
+	swapChainDesc.BufferCount = 1;					// we have only one back buffer
+	swapChainDesc.BufferDesc.Width = screenWidth;	// set the resolution of the back buffer
+	swapChainDesc.BufferDesc.Height = screenHeight;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	// use a simple 32-bit surface 
+	swapChainDesc.SampleDesc.Count = 1;				// setup of the multisampling
+	swapChainDesc.SampleDesc.Quality = 0;
+	
+	if (m_vsync_enabled)	// if we use a vertical synchronization
+	{
+		Log::Get()->Debug(THIS_FUNC, "VSYNC enabled mode");
+		swapChainDesc.BufferDesc.RefreshRate.Numerator = numerator;		// set the refresh rate of the back buffer
+		swapChainDesc.BufferDesc.RefreshRate.Denominator = denominator;
+	}
+	else					// we don't use a vertical synchronization
+	{
+		Log::Get()->Debug(THIS_FUNC, "VSYNC disabled mode");
+		swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;		// set the refresh rate of the back buffer
+		swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+	}
+
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	// use the back buffer as the render target output
+	swapChainDesc.OutputWindow = hwnd;								// set the current window
+	swapChainDesc.Windowed = !FULL_SCREEN;							// set or not a full screen mode;
+	
+	swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;	// a rasterizer method to render an image on a surface
+	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;	// how to scale an image to fit it to the screen resolution
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;			// discard the content of the back buffer after presenting
+	swapChainDesc.Flags = 0;
+
+	featureLevel = D3D_FEATURE_LEVEL_11_0;	// tell DirectX which feature level we want to use
+
+	// Create the swap chain, device and device context
+	hr = D3D11CreateDeviceAndSwapChain(nullptr,
+										D3D_DRIVER_TYPE_HARDWARE,
+										NULL,
+										createDeviceFlags,
+										&featureLevel,
+										1,
+										D3D11_SDK_VERSION,
+										&swapChainDesc,
+										&m_pSwapChain,
+										&m_pDevice,
+										nullptr,
+										&m_pDeviceContext);
+	if (FAILED(hr))
+	{
+		Log::Get()->Error(THIS_FUNC, "can't create the swap chain");
+		return false;
+	}
+
+
+	// ------------------------------------------------------------------------- //
+	//                   CREATE THE RENDER TARGET VIEW							 //
+	// ------------------------------------------------------------------------- //
+
+	// get the buffer from the swap chain which we will use as a 2D Texture
+	hr = m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBuffer);
+	if (FAILED(hr))
+	{
+		Log::Get()->Error(THIS_FUNC, "can't get a buffer from the swap chain");
+		return false;
+	} 
+
+	// create a render target view 
+	hr = m_pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &m_pRenderTargetView);
+	_RELEASE(pBackBuffer);
+	if (FAILED(hr))
+	{
+		Log::Get()->Error(THIS_FUNC, "can't create a render target view");
+		return false;
+	}
+
+
+	// ------------------------------------------------------------------------- //
+	//             CREATE THE DEPTH STENCIL BUFFER,                              //
+	//          DEPTH STENCIL STATE AND DEPTH STENCIL VIEW						 //
+	// ------------------------------------------------------------------------- //
+
+	// Intialize the depth stencil buffer description
+	ZeroMemory(&depthStencilBufferDesc, sizeof(D3D11_TEXTURE2D_DESC));
+
+	// Setup the depth stencil buffer description
+	depthStencilBufferDesc.Width = screenWidth;
+	depthStencilBufferDesc.Height = screenHeight;
+	depthStencilBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;	// 24 bits for the depth and 8 bits for the stencil
+	depthStencilBufferDesc.MipLevels = 1;
+	depthStencilBufferDesc.ArraySize = 1;
+	depthStencilBufferDesc.SampleDesc.Count = 1;
+	depthStencilBufferDesc.SampleDesc.Quality = 0;
+	depthStencilBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthStencilBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthStencilBufferDesc.CPUAccessFlags = 0;
+	depthStencilBufferDesc.MiscFlags = 0;
+
+	// Create the depth stencil buffer
+	hr = m_pDevice->CreateTexture2D(&depthStencilBufferDesc, nullptr, &m_pDepthStencilBuffer);
+	if (FAILED(hr))
+	{
+		Log::Get()->Error(THIS_FUNC, "can't create the depth stencil buffer");
+		return false;
+	}
+
+	// Initialize the depth stencil state description
+	ZeroMemory(&depthStencilDesc, sizeof(D3D11_DEPTH_STENCIL_DESC));
+	
+	// Setup the depth stencil state description
+	depthStencilDesc.DepthEnable = true;	// enable depth testing
+	depthStencilDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;	// a part of the depth buffer to writing
+	depthStencilDesc.DepthFunc = D3D11_COMPARISON_LESS;	// a function to compare source depth data against exiting (destination) depth data
+
+	depthStencilDesc.StencilEnable = true;	// enable stencil testing
+	depthStencilDesc.StencilWriteMask = 0xFF;	// a part of the stencil buffer to write
+	depthStencilDesc.StencilReadMask = 0xFF;	// a part of the staneil buffer to read
+
+	// set operations if pixel is front facing
+	depthStencilDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;	// not change buffer values if stencil testing is failed
+	depthStencilDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;	// increment the buffer values if depth testing is failed
+	depthStencilDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;	// not change buffer values if stencil testing and depth testing are passed
+	depthStencilDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;	// a function to compare source stencil data against exiting (destination) stencil data
+
+	// set operations if pixel is back facing
+	depthStencilDesc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;	// not change buffer values if stencil testing is failed
+	depthStencilDesc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;	// decrement the buffer values if depth testing is failed
+	depthStencilDesc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;	// not change buffer values if stencil testing and depth testing are passed
+	depthStencilDesc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;	// a function to compare source stencil data against exiting (destination) stencil data
+
+	// Create a depth stencil state
+	hr = m_pDevice->CreateDepthStencilState(&depthStencilDesc, &m_pDepthStencilState);
+	if (FAILED(hr))
+	{
+		Log::Get()->Error(THIS_FUNC, "can't create a depth stencil state");
+		return false;
+	}
+
+
+	// set the render target view
+	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, m_pDepthStencilView);
 
 	return true;
 }
