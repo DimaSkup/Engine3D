28d5a689d2c38380a71f854a2f3ec73b5ee27c41
unknown <dimaskupejko@gmail.com>
2022-04-06 18:19:58 +0300

    Tutorial 4: #22: revising of the color shader class (day 2)

--- cameraclass.cpp
+++ cameraclass.cpp
@@ -72,15 +72,15 @@ void CameraClass::Render(void)
 
 	// Create the rotation matrix from the yaw, pitch, and roll values
 	//D3DXMatrixRotationYawPitchRoll(&rotationMatrix, yaw, pitch, roll);
-	D3DXMatrixRotationY(&rotationMatrix, angle);
+	//D3DXMatrixRotationY(&rotationMatrix, angle);
 
 	// Transform the lookAt and up vector by the rotation matrix
 	// so the view is correctly rotated at the origin
-	D3DXVec3TransformCoord(&lookAt, &lookAt, &rotationMatrix);
+	//D3DXVec3TransformCoord(&lookAt, &lookAt, &rotationMatrix);
 	//D3DXVec3TransformCoord(&up, &up, &rotationMatrix);
 
 	// Translate the rotated camera position to the location of the viewer
-	lookAt = position + lookAt;
+	//lookAt = position + lookAt;
 
 	// Finally create the view matrix from the three updated vectors
 	D3DXMatrixLookAtLH(&m_viewMatrix, &position, &lookAt, &up);
--- colorshaderclass.h
+++ colorshaderclass.h
@@ -1,17 +1,18 @@
 /////////////////////////////////////////////////////////////////////
 // Filename:       colorshaderclass.h
-// Description:    We use this class to invoke HLSL shaders to 
-//                 draw our 3D models which are on the GPU
-// 
-// Revising:       05.04.22
+// Description:    We use this class to invoke HLSL shaders 
+//                 for drawing our 3D models which are on the GPU
+//
+// Revising:       06.04.22
 /////////////////////////////////////////////////////////////////////
-
+#pragma once
 
 //////////////////////////////////
 // INCLUDES
 //////////////////////////////////
 #include "includes.h"
 #include "log.h"
+
 
 //////////////////////////////////
 // Class name: ColorShaderClass
@@ -28,88 +29,10 @@ public:
 	bool Render(ID3D11DeviceContext*, int, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX);
 
 private:
-	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);	// compilation and setting of shaders
 	void ShutdownShader(void);
 
-	bool SetShaderParameters(ID3D11DeviceContext*, D3DXMATRIX, D3DMATRIX, D3DXMATRIX);	// here we initialize a shader constant buffer
-	void RenderShaders(ID3D11DeviceContext*, int);
-
-	HRESULT CompileShaderFromFile(WCHAR*, LPCSTR, LPCSTR, ID3DBlob**);
-	HRESULT CompileShaderFromFile(WCHAR*, LPCSTR, LPCSTR, ID3DBlob**);
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-/////////////////////////////////////////////////////////////////////
-// Filename:    colorshaderclass.h
-// Description: this class we use to invoke our HLSL shaders for drawing
-//              the 3D models that are on the GPU
-// 
-// Revising:    29.03.22
-/////////////////////////////////////////////////////////////////////
-#pragma once
-
-//////////////////////////////////
-// INCLUDES
-//////////////////////////////////
-#include "includes.h"
-#include "log.h"
-
-using namespace std;
-
-
-//////////////////////////////////
-// Class name: ColorShaderClass
-//////////////////////////////////
-class ColorShaderClass
-{
-public:
-	ColorShaderClass(void);
-	ColorShaderClass(const ColorShaderClass& another);
-	~ColorShaderClass(void);
-
-	bool Initialize(ID3D11Device*, HWND);	// calls initialization function for the shaders
-	void Shutdown(void);
-	bool Render(ID3D11DeviceContext*, int, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX);
-
-private:
-	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
-	void ShutdownShader(void);
-
-	bool SetShaderParameters(ID3D11DeviceContext*, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX);
+	bool SetShaderParameters(ID3D11DeviceContext*, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX);	// here we setup the constant shader buffer
 	void RenderShader(ID3D11DeviceContext*, int);
 
 	HRESULT CompileShaderFromFile(WCHAR*, LPCSTR, LPCSTR, ID3DBlob**);
@@ -128,4 +51,3 @@ private:
 	ID3D11Buffer*       m_pMatrixBuffer;
 };
 
-*/
