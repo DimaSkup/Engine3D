099676815c96f3110d037c8d4e0aeaf828b9c167
unknown <dimaskupejko@gmail.com>
2022-04-09 16:40:16 +0300

    Tutorial 5: #24: not finished; development of the texture shaders and TextureClass class;

---
+++ textureclass.cpp
@@ -0,0 +1,53 @@
+
+////////////////////////////////////////////////////////////////////
+// Filename: textureclass.cpp
+////////////////////////////////////////////////////////////////////
+#include "textureclass.h"
+
+TextureClass::TextureClass(void)
+{
+	m_texture = nullptr;
+}
+
+TextureClass::TextureClass(const TextureClass& other)
+{
+}
+
+TextureClass::~TextureClass(void)
+{
+}
+
+// Loads the texture file into the shader resource variable called m_texture.
+// The texture can now be used to render with
+bool TextureClass::Initialize(ID3D11Device* device, WCHAR* filename)
+{
+	HRESULT hr = S_OK;
+
+	// Load the texture in
+	hr = D3DX11CreateShaderResourceViewFromFile(device, filename, 
+                                                nullptr, nullptr,
+                                                &m_texture, nullptr);
+	if (FAILED(hr))
+	{
+		Log::Get()->Error(THIS_FUNC, "can't create the shader resource view from file");
+		return false;
+	}
+
+	return true;
+}
+
+// Releases the texture resource if it has been loaded and then sets the poitner to nullptr
+void TextureClass::Shutdown(void)
+{
+	// Release the texture resource
+	_RELEASE(m_texture);
+
+	return;
+}
+
+// Called by other objects that need access to the texture shader resource so that
+// they can use the texture for rendering
+ID3D11ShaderResourceView* TextureClass::GetTexture(void)
+{
+	return m_texture;
+}
---
+++ textureclass.h
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////
+// Filename:      textureclass.h
+// Description:   Encapsulates the loading, unloading, and accessing
+//                of a single texture resource. For each texture 
+//                needed an object of this class to be instantiated.
+//
+// Revising:      09.04.22
+////////////////////////////////////////////////////////////////////
+#pragma once
+
+//////////////////////////////////
+// INCLUDES
+//////////////////////////////////
+#include <d3dx11tex.h>
+#include "includes.h"
+#include "log.h"
+
+//////////////////////////////////
+// Class name: TextureClass
+//////////////////////////////////
+class TextureClass
+{
+public:
+	TextureClass(void);
+	TextureClass(const TextureClass&);
+	~TextureClass(void);
+
+	bool Initialize(ID3D11Device*, WCHAR*); // loads texture from a given file
+	void Shutdown(void);                    // unloads texture when it is no longer needed
+
+	ID3D11ShaderResourceView* GetTexture(void);	// returns a pointer to the texture resource so it can be used for rendering by shaders
+
+private:
+	ID3D11ShaderResourceView* m_texture;
+};
